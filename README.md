1bb88a02d07a651e8b71858d1afc5800b7054f34
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-13 14:03:57 +0900

    Enhance: can specify more than 2 target as test script

--- README.md
+++ README.md
@@ -5,18 +5,23 @@
 # SYNOPSYS
 
     # Run as script
-    loop=1000 bb.bash "<bash code A>" "<bash code B>"
+    loop=1000 bb.bash "<bash code A>" [...]
 
     # Run as function
     . bb.bash
-    loop=1000 bb "<bash code A>" "<bash code B>"
+    loop=1000 bb "<bash code A>" [...]
 
     # Debug
-    BB_DEBUG=1 bb "<bash code A>" "<bash code B>"
+    BB_DEBUG=1 bb "<bash code A>" [...]
 
 # DESCRIPTION
 
 A Benchmark script for Bash.
+
+All arguments for this script or function `bb` are taken as bash codes to test.
+
+You can test only single command.
+And you can compare any number of commands.
 
 # AUTHORS
 
--- bb.bash
+++ bb.bash
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-__a=$1
-__b=$2
-
 _bb_bench() {
   local script=$1
   local loop=$2
@@ -14,27 +11,22 @@ _bb_bench() {
 }
 
 bb() {
-  local a=$1
-  local b=$2
   local loop=${loop:-1000}
+  local sbj
 
-  if [ $BB_DEBUG ]; then
-    echo "Exec '${a}'"
-    bash -cx "$a"
-    echo "Exec '${b}'"
-    bash -cx "$b"
-  else
-    _bb_bench "${a}" ${loop}
-    _bb_bench "${b}" ${loop}
-  fi
+  for sbj in "$@"; do
+    if [ $BB_DEBUG ]; then
+      echo "Exec '${sbj}'"
+      bash -cx "${sbj}"
+    else
+      _bb_bench "${sbj}" ${loop}
+    fi
+  done
 }
 
 if [ "$0" = "${BASH_SOURCE}" ]; then
-  bb "${__a}" "${__b}"
+  bb "$@"
 fi
-
-unset __a
-unset __b
 
 if [ $BB_NO_FUNCTION ]; then
   unset -f _bb_bench
@@ -52,18 +44,23 @@ B<bb> - A Benchmark tool for Bash script
 =head1 SYNOPSYS
 
     # Run as script
-    loop=1000 bb.bash "<bash code A>" "<bash code B>"
+    loop=1000 bb.bash "<bash code A>" [...]
 
     # Run as function
     . bb.bash
-    loop=1000 bb "<bash code A>" "<bash code B>"
+    loop=1000 bb "<bash code A>" [...]
 
     # Debug
-    BB_DEBUG=1 bb "<bash code A>" "<bash code B>"
+    BB_DEBUG=1 bb "<bash code A>" [...]
 
 =head1 DESCRIPTION
 
 A Benchmark script for Bash.
+
+All arguments for this script or function C<bb> are taken as bash codes to test.
+
+You can test only single command.
+And you can compare any number of commands.
 
 =head1 AUTHORS
 
